222d16e7f27406c3e8d5416e066d3d822f0ec1db
Celine_Potin <116295408+CELINEPOTIN@users.noreply.github.com>
2023-06-23 12:32:07 +0200

    Add files via upload

--- ProjetProLine/ProjetProLine/appproline/settings.py
+++ ProjetProLine/ProjetProLine/appproline/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'ProjetProLine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Potin_bloc3',
+        'HOST':'mysql-potin.alwaysdata.net',
+        'USER':'root',
+        'PASSWORD':'Admin',
+        'PORT':'3306'
+    }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
